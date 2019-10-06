dc2dd12ebd2f9a73c1cdbcb8b29c447695a650b5
Marco Volkert <marco.volkert24@gmx.de>
2019-10-06 13:32:50 +0200

    create init file

---
+++ __init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+collection of pdf tools
+"""
+
+__author__ = "Marco Volkert"
+__copyright__ = "Copyright 2018, Marco Volkert"
+__email__ = "marco.volkert24@gmx.de"
+__status__ = "Development"
+
+__all__ = ["pdftools"]
